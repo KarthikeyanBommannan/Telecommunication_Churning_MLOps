acb8390cb767cc1623cc13e2ae726288732fcb20
KarthikeyanBommannan <karthibommannan@gmail.com>
2023-09-25 15:08:39 +0530

    a new commit

---
+++ Telecommunication_Churning_MLOps/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Baesd on the data provided will be able to tell that the customer is like to stay or churn',
+    author='karthikeyanbommannan',
+    license='MIT',
+)
