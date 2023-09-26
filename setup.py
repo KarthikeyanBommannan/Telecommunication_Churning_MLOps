06f86cf8ebdcedd72aaf42f6ab6fdc19b78c2770
KarthikeyanBommannan <karthibommannan@gmail.com>
2023-09-26 17:34:14 +0530

    changes

--- setup.py
+++ setup.py
@@ -31,13 +31,4 @@ setuptools.setup(
 
 
 
-from setuptools import find_packages, setup
 
-setup(
-    name = 'src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Telecommunication Churn Project',
-    author='KarthikeyanBommannan',
-    license='MIT',
-)
